182af7abddeb828f67723e22eca72d4bf734dd06
Michał <michalmalicki@op.pl>
2022-01-25 17:00:25 +0100

    finished stoper app

---
+++ js/main.js
@@ -0,0 +1,93 @@
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
+const archBtn = document.querySelector('.history');
+const stopwatch = document.querySelector('.stopwatch');
+const time = document.querySelector('.time');
+const timeList = document.querySelector('.time-list');
+
+const infoBtn = document.querySelector('.info');
+const modalShadow = document.querySelector('.modal-shadow');
+const closeBtn = document.querySelector('.close');
+let countTime;
+let minutes = 0;
+let seconds = 0;
+
+let timesArr = [];
+
+const handleStart = () => {
+	clearInterval(countTime);
+	countTime = setInterval(() => {
+		if (seconds < 9) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:0${seconds}`;
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++;
+			stopwatch.textContent = `${minutes}:${seconds}`;
+		} else {
+			minutes++;
+			seconds = 0;
+			stopwatch.textContent = `${minutes}:00`;
+		}
+	}, 100);
+};
+
+const handlePause = () => {
+	clearInterval(countTime);
+};
+
+const handleStop = () => {
+	time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`;
+
+	if (stopwatch.textContent !== '0:00') {
+		time.style.visibility = 'visible';
+		timesArr.push(stopwatch.textContent);
+	}
+
+	clearStuff();
+};
+
+const handleReset = () => {
+	time.style.visibility = 'hidden';
+	timesArr = [];
+	clearStuff();
+};
+
+const clearStuff = () => {
+	clearInterval(countTime);
+	stopwatch.textContent = '0:00';
+	timeList.textContent = '';
+	seconds = 0;
+	minutes = 0;
+};
+
+const showHistory = () => {
+	timeList.textContent = '';
+	let num = 1;
+	timesArr.forEach(time => {
+		const newTime = document.createElement('li');
+		newTime.innerHTML = `Pomiar nr ${num}:<span>${time}</span>`;
+		timeList.append(newTime);
+		num++;
+	});
+};
+
+const showModal = () => {
+	if (!(modalShadow.style.display === 'block')) {
+		modalShadow.style.display = 'block';
+	} else {
+		modalShadow.style.display = 'none';
+	}
+
+	modalShadow.classList.toggle('modal-animation');
+};
+
+startBtn.addEventListener('click', handleStart);
+pauseBtn.addEventListener('click', handlePause);
+stopBtn.addEventListener('click', handleStop);
+resetBtn.addEventListener('click', handleReset);
+archBtn.addEventListener('click', showHistory);
+infoBtn.addEventListener('click', showModal);
+closeBtn.addEventListener('click', showModal);
+window.addEventListener('click' , e => e.target === modalShadow ? showModal(): false);
